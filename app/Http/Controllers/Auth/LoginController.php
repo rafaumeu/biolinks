f2d47a41403356be81653bfb39a314d760de3375
Rafael Dias Zendron <mmmarckos@gmail.com>
2026-01-27 18:23:54 -0300

    feat(auth): implement initial login system
    Implementation:
    - Create `LoginController` with `index` method
    - Create basic `auth.login` view
    - Register `/login` route in `web.php`
    
    Closes #1

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class LoginController extends Controller
+{
+    public function index(): View{
+        return view("auth.login");
+    }
+    public function login(): void {
+        dd(request()->all());
+    }
+}
