77e928a672a78919266f88d98542db54f6a7b89a
Rafael Dias Zendron <mmmarckos@gmail.com>
2026-01-27 18:23:54 -0300

    feat(auth): implement initial login system
    Implementation:
    - Create `LoginController` with `index` method
    - Create basic `auth.login` view
    - Register `/login` route in `web.php`
    
    Closes #1

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/login', [LoginController::class,'index' ]);
