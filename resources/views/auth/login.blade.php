452fbe64e6d2314b824c62af12d5cd3c924c21e8
Rafael Dias Zendron <mmmarckos@gmail.com>
2026-01-28 00:55:54 -0300

    feat(auth): refactor login validation to Form Request
    Implementation:
    - Create `MakeLoginRequest` with validation rules and auth logic
    - Update `LoginController` to use `MakeLoginRequest` and DI
    - Move authentication logic to request class
    
    Context:
    - Refactors inline validation to dedicated class
    - Fixes return type checks
    - Closes #2 (Form Validation)

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,9 +1,24 @@
 <div>
     <h1>Login</h1>
+    @if($mensagem = session()->get('mensagem'))
+        <p>{{ $mensagem }}</p>
+    @endif
     <form action="/login" method="POST">
         @csrf
-        <input name="email" type="email" placeholder="Email" value="{{ old('email') }}">
-        <input name="password" type="password" placeholder="Senha">
+        <div>
+            <input name="email" type="email" placeholder="Email" value="{{ old('email') }}">
+            @error("email")
+                <span>{{ $message }}</span>
+            @enderror
+        </div>
+        <br>
+        <div>
+            <input name="password" type="password" placeholder="Senha">
+            @error("password")
+                <span>{{ $message }}</span>
+            @enderror
+        </div>
+        <br>
         <button>Login</button>
     </form>
 </div>
