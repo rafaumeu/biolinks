2a833b2e2d991e9900cb24165b2b098799cb119c
Rafael Dias Zendron <mmmarckos@gmail.com>
2026-01-30 00:39:04 -0300

    feat(links): implement link edit and update functionality
    Implementation:
    - Implemented `edit` and `update` methods in `LinkController`
    - Created `links.edit` view with validation feedback
    - Added Routes for edit (GET) and update (PUT)
    - Updated Dashboard to include links to edit page
    - Utilized `UpdateLinkRequest` for validation
    
    Context:
    - Allows users to modify their existing links
    - Completes the Update part of CRUD operations
    - Progress on Issue #39
    
    Closes #39

---
+++ resources/views/links/edit.blade.php
@@ -0,0 +1,25 @@
+<div>
+    <h1>Editar um link : {{ $link->id }}</h1>
+    @if($mensagem = session()->get('mensagem'))
+        <p>{{ $mensagem }}</p>
+    @endif
+    <form action="{{route('links.edit', $link)}}" method="POST">
+        @csrf
+        @method('PUT')
+        <div>
+            <input name="link" type="text" placeholder="link" value="{{ old('link', $link->link) }}">
+            @error("link")
+                <span>{{ $message }}</span>
+            @enderror
+        </div>
+        <br>
+        <div>
+            <input name="name" type="text" placeholder="Nome" value="{{ old('name', $link->name) }}">
+            @error("name")
+                <span>{{ $message }}</span>
+            @enderror
+        </div>
+        <br>
+        <button>Salvar</button>
+    </form>
+</div>
