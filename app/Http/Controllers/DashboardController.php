078036cc2d4aad56fdd21ee77a9631806cfba788
Rafael Dias Zendron <mmmarckos@gmail.com>
2026-01-30 00:03:21 -0300

    feat(links): implement link management and git hooks
    Implementation:
    - Created `Link` model and `links` table migration with user_id
    - Implemented `LinkController` (create/store) and `DashboardController`
    - Added User-Link relationships (User hasMany Links)
    - Created `LinkFactory`, `LinkSeeder` and `UserSeeder` for dummy data
    - Added `laravel-debugbar` to dev dependencies
    - Configured Git Hooks (Husky, Lint-Staged, Commitlint) for code quality
    
    Context:
    - Sets up the foundation for the usage of Links
    - Ensures links are securely associated with the authenticated user
    - Ensfoces Pint and PHPStan on pre-commit
    
    Closes #10, #11, #12, #13

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,13 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
-use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    public function __invoke() {
+    public function __invoke()
+    {
         /** @var User $user */
-        $user = auth()->user()->links;
-        return view('dashboard');
+        $user = auth()->user();
+
+        return view('dashboard', ['links' => $user->links]);
     }
 }
--- database/seeders/LinkSeeder.php
+++ database/seeders/LinkSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Link;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class LinkSeeder extends Seeder
@@ -11,6 +13,10 @@ class LinkSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        User::all()->each(function (User $user) {
+            Link::factory()->count(random_int(5, 10))->create([
+                'user_id' => $user->id,
+            ]);
+        });
     }
 }
